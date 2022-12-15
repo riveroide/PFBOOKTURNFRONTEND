0d02969a7c1faa36de79dc977afa79e709cdb01f
Pepo2405 <ignacioniglesias@gmail.com>
2022-12-15 19:22:01 -0300

    rutas y controllers

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -1,16 +1,21 @@
 import express from "express";
+import clients from "./routes/clients";
+import business from "./routes/business";
+import services from"./routes/services";
+import booking from "./routes/booking"
+
 const app = express();
 app.use(express.json()); //middleware que transforma req.body en json
 
 const PORT = 3000;
 
-app.get("/ping", (_req, res) => {
-  res.send("pong");
-});
+app.use("/clients", clients);
 
-app.get("/pingo", (_req, res) => {
-  res.send("pong");
-});
+app.use("/business", business);
+
+app.use("/services",services);
+
+app.use("/booking",booking)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
