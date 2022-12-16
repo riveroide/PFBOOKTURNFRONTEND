6b737422199da16e9930a34a33d5df5c5d007046
riveroide <nicolasrivero1994@gmail.com>
2022-12-16 01:59:37 -0400

    get y post clients

--- BackEnd/src/routes/clients.ts
+++ BackEnd/src/routes/clients.ts
@@ -1,8 +1,30 @@
 import {Router} from "express"
+import { getAllClients } from "../controllers/clients";
 const router = Router();
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
+router.get("/", async (_req,res) => {
+    try {
+    const result = await getAllClients();
+    return res.json(result)
+    } catch (error) {
+        return "error en route"
+    }
+    
+})
 
-
+router.post("/", async (req, res) => {
+    const { email, user } = req.body
+    if (!user || !email) throw new Error ("Faltan datos")
+    try {
+        const newclient = await prisma.client.create({data: req.body})
+        console.log(newclient)
+        res.status(200).send(newclient)
+    } catch (error) {
+        res.status(404).send(error)
+    }
+})
 
 
 export default router;
