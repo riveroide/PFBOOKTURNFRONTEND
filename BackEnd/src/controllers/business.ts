fcb866c54bc2e0fb5f2e2c9302dd8500063c6e22
Pepo2405 <ignacioniglesias@gmail.com>
2022-12-16 19:54:25 -0300

    ruta put business

--- BackEnd/src/controllers/business.ts
+++ BackEnd/src/controllers/business.ts
@@ -2,12 +2,23 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const getAllBusiness = async () => {
-    try {
-        const result = await prisma.business.findMany()
-        return result
-    } catch (error) {
-        return error
-    }
-    
-    
-}
+  try {
+    const result = await prisma.business.findMany();
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const upDateBusiness = async (body: any, id: number) => {
+  try {
+    const business = await prisma.business.update({
+      where: { id },
+      data: body,
+    });
+    return business;
+  } catch (error) {
+    console.log("error", error);
+    throw error;
+  }
+};
