b042eec2a61a27bb3dcfd39bdf2e043493df5552
AgustinSG01 <sousagodoyagustin@gmail.com>
2022-12-16 06:00:32 +0100

    get service hecho

--- BackEnd/src/controllers/services.ts
+++ BackEnd/src/controllers/services.ts
@@ -1,2 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+
+
+export const getService = async () => {
+    try {
+        const getServices = prisma.services.findMany()
+        return getServices
+    } catch (error) {
+        return "error getService controller"
+    }
+}
--- BackEnd/src/routes/services.ts
+++ BackEnd/src/routes/services.ts
@@ -1,8 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 import {Router} from "express"
+import {getService } from "../controllers/services";
 const router = Router();
 
+router.get("/", async (_, res) => {
+   const services = await getService()
+   services.length ? res.status(200).send(services) : res.status(404).send("no hay services")
+})
 
-
-
+router.post("/", async (req, res) => {
+    const {name, price} = req.body
+    if (!name || !price) throw new Error ("Faltan datos")
+    try {
+        const service = await prisma.services.create({data: req.body})
+        console.log(service)
+        res.status(200).send(service)
+    } catch (error) {
+        res.status(404).send(error)
+    }
+})
 
 export default router;
