3a0b501c1e9dbf43570545404da415ebc79e2f8f
riveroide <nicolasrivero1994@gmail.com>
2022-12-16 03:49:40 -0400

    get booking

--- BackEnd/src/controllers/booking.ts
+++ BackEnd/src/controllers/booking.ts
@@ -1,2 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+
+export const getAllBookings = async () => {
+    try {
+        const result = await prisma.booking.findMany()
+        return result
+    } catch (error) {
+        return error
+    }
+    
+    
+}
