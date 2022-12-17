02eade6cbd45d03079a464d419593ad2f8c82f14
ivorojas <ivo.2001@hotmail.com>
2022-12-16 22:18:43 -0300

    asdf

--- FrontEnd/components/NavBarClients.jsx
+++ FrontEnd/components/NavBarClients.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import styles from '../styles/NavBarClients.module.css'
+import stylesNavBar from '../styles/NavBarClients.module.css'
 
 export default function NavBarClients() {
   return (
-    <div className={styles.NavbarClientsContainer} >
-   <div className={styles.logo} >
-   <h4> 
+    <div className={stylesNavBar.NavbarClientsContainer} >
+   <div className={stylesNavBar.logo} >
+   
    bookturn
-    </h4>
+  
    </div>
      
-        <div className={styles.buttonsLogin} >
-        <button className={styles.acceder} >acceder</button>
-        <button className={styles.resgistrarse} >Registrarse</button>
+        <div className={stylesNavBar.buttonsLogin} >
+        <button className={stylesNavBar.acceder} >acceder</button>
+        <button className={stylesNavBar.resgistrarse} >Registrarse</button>
         </div>
    
         </div>
