03170c6e92b81ad6fad17b739a0c2898fb51c5e4
tomasgp30 <113186486+tomasgp30@users.noreply.github.com>
2022-12-17 16:53:30 -0300

    profile

--- FrontEnd/pages/profile.tsx
+++ FrontEnd/pages/profile.tsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import styles from './profile.module.css'
 
 const profile = () => {
   return (
-    <div>profile</div>
+    <div>
+      <div>
+        <nav className={styles.navBar}>
+          <a href="#" className={styles.navTitle}>BOOKTURN</a>
+          <input type="text" className={styles.search} />
+          <a href="#" className={styles.navTitle}>Cerrar sesion</a>
+        </nav>
+      </div>
+      <div>
+        <div>Profile</div>
+      </div>
+      <ul>
+        <li>
+          <a href="#">
+            <span>editar informacion</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span>metodo de pago</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span>historial</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span>favoritos</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span>opciones</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span>eliminar cuenta</span>
+          </a>
+        </li>
+      </ul>
+    </div>
   )
 }
 
