6d15c1b8f05b946d0aae30a5780e015de2bdf184
CamiloCVC <camicastellanos0602@gmail.com>
2022-12-24 00:32:50 -0500

    home business schema 1

---
+++ FrontEnd/pages/business/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavbarBussines from "../../components/HomeBussines/NavbarBussines/NavbarBussines.jsx";
+import LandingInfoBussines from "../../components/HomeBussines/LandingInfoBussines/LandingInfoBussines.jsx";
+
+const index = () => {
+  return (
+    <div>
+      <NavbarBussines/>
+      <LandingInfoBussines/>
+    </div>
+  );
+};
+
+export default index;
