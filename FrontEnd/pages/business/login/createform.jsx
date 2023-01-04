dd24d9ef9a8fca0f5f75d59d630eff4d3599a666
CamiloCVC <camicastellanos0602@gmail.com>
2023-01-04 17:02:27 -0500

    avance formulario

--- FrontEnd/pages/business/login/createform.jsx
+++ FrontEnd/pages/business/login/createform.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import { useDispatch } from "react-redux";
+
 
 const createform = () => {
+  const dispatch = useDispatch()
+  const [data, setData] = useState(
+    {
+      user: "",
+      // email: ""
+    }
+  )
+
+  const handlerChange=(e)=>{
+    e.preventDefault(),
+    setData({...data, [e.target.name]: e.target.value})
+    // console.log(data)
+  }
+
+  const handlerSubmit=(e)=>{
+    e.preventDefault(e)
+    console.log(data)
+    dispatch(postBusiness(data))
+  }
+
   return (
     <div>
       <h1>SOY EL FORMULARIO DE CREACION DE EMPRESA</h1>
+      <div>
+        <form onSubmit={(e)=>handlerSubmit(e)}>
+          <p>Ingrese nombre</p>
+          <input name="user" value={data.name} type="text" onChange={(e)=>{handlerChange(e)}}/>
+          {/* <input name="email" value={data.email} type="text" onChange={(e)=>{handlerChange(e)}}/> */}
+          <input type="submit" />
+        </form>
+      </div>
       <Link href="/business/login"><button>Volver al login</button></Link>
     </div>
   );
--- FrontEnd/redux/actions/services/postServices.js
+++ FrontEnd/redux/actions/services/postServices.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export const postServices = (data) => () => {
-  axios.post("http://localhost:3000/services", data).catch((error) => error);
+  try {
+    axios.post("http://localhost:3000/services", data)
+  } catch (error) {
+    console.log(error.message)
+  }
 };
