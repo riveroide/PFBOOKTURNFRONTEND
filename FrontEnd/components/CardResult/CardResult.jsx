b6b5cd2abc2cdba8a9731d1c9bcf1c6541c71fc7
AgustinSG01 <sousagodoyagustin@gmail.com>
2023-01-05 02:25:32 +0100

    ya aparecen cosas en results, falta ponerlo lindo

--- FrontEnd/components/CardResult/CardResult.jsx
+++ FrontEnd/components/CardResult/CardResult.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import stylesCard from '../CardResult/CardResult.module.css'
-export default function CardResult() {
+import React from "react";
+import stylesCard from "../CardResult/CardResult.module.css";
+export default function CardResult({ name, services }) {
   return (
-    <div className={stylesCard.cardContainer} >
-
-      <div className={stylesCard.imagenContainer} >
-        <div className={stylesCard.imagenPrueba} >imagen</div>
+    <div className={stylesCard.cardContainer}>
+      <div className={stylesCard.imagenContainer}>
+        <div className={stylesCard.imagenPrueba}>imagen</div>
       </div>
 
-      <div className={stylesCard.contenidoCard} >
-        <p className={stylesCard.nombreEmpresa} >Nombre de la empresa</p>
-  
+      <div className={stylesCard.contenidoCard}>
+        <p className={stylesCard.nombreEmpresa}> {name} </p>
+        {services?.map((e) => {
+          return (
+            <tr>
+              <td> {e.name} </td>
+              <td> {e.time} </td>
+              <td> {e.price} </td>
+            </tr>
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }
