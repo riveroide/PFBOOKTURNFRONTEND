8273d182d41df6641b06771a61f26113004851dc
AgustinSG01 <sousagodoyagustin@gmail.com>
2023-01-05 21:03:49 +0100

    para que pepo pueda laburar

--- FrontEnd/redux/actions/business/getBusiness.js
+++ FrontEnd/redux/actions/business/getBusiness.js
@@ -2,17 +2,25 @@ import { getAllBusiness, getBusinessId } from "redux/reducers/businessSlice";
 import axios from "axios";
 
 export const getBusiness = () => (dispatch) => {
-  axios("http://localhost:3001/business")
+  axios("http://localhost:1337/api/businesses?populate=*")
     .then((res) => {
-      dispatch(getAllBusiness(res.data));
+      dispatch(getAllBusiness(res.data.data));
     })
     .catch((error) => console.log(error));
 };
 
 export const getBusinessById = (id) => (dispatch) => {
   axios(`http://localhost:3001/business/${id}`)
-  .then(res => {
-      dispatch(getBusinessId(res.data))
+    .then((res) => {
+      dispatch(getBusinessId(res.data));
+    })
+    .catch((error) => console.log(error));
+};
+
+export const getBusinessByName = (name) => (dispatch) => {
+  axios("http://localhost:1337/api/businesses?filters[name][$containsi]=" + name)
+  .then((res) => {
+    dispatch(getAllBusiness(res.data.data))
   })
-  .catch(error => console.log(error))
-}
+
+};
