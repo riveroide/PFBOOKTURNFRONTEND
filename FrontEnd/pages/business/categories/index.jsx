6ebcc43befe5241ee099effd107832d58f13cb44
agustinsavoy1 <agustinsavoy1@gmail.com>
2023-01-08 22:48:52 -0300

    Footer component

--- FrontEnd/pages/business/categories/index.jsx
+++ FrontEnd/pages/business/categories/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from "../../../components/Navbar/Navbar"
 import NavbarCategories from 'components/NavbarCategories/NavbarCategories'
-import FooterBussines from "components/FooterBussines/FooterBussines.jsx";
+import FooterBussines from "../../../components/FooterBussines/FooterBussines";
 
 
 const index = () => {
--- FrontEnd/pages/results/index.jsx
+++ FrontEnd/pages/results/index.jsx
@@ -1,8 +1,9 @@
 
 import React from 'react'
 import Navbar from "../../components/Navbar/Navbar"
-//import CardResult from '../../components/CardResult/CardResult'
+import CardResult from '../../components/CardResult/CardResult'
 import Results from '../../components/Results/Results'
+import Footer from "../../components/FooterBussines/FooterBussines"
 
 export default function index() {
   return (
@@ -12,7 +13,7 @@ export default function index() {
           <div>
             <Results />
           </div>
-
+          <Footer/>
     </div>
   );
 }
