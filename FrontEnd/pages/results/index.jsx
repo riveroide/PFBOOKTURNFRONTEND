3c298bc48c295cc8ea1bcec5d5f3296180f48c02
riveroide <nicolasrivero1994@gmail.com>
2023-01-08 23:27:13 -0400

    back borrado y parte de organizacion del boilerplate del front

--- FrontEnd/pages/results/index.jsx
+++ FrontEnd/pages/results/index.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import Navbar from "../../components/Navbar/Navbar"
+import Navbar2 from "../../components/Navbar/Navbar2"
 import CardResult from '../../components/CardResult/CardResult'
 import Results from '../../components/Results/Results'
 import Footer from "../../components/FooterBussines/FooterBussines"
@@ -9,7 +9,7 @@ export default function index() {
   return (
     <div>
 
-           <Navbar/>
+           <Navbar2/>
           <div>
             <Results />
           </div>
