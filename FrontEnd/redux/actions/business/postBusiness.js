5593cd73b35ba9992fadfd32f0c1fcd77d1a98c5
riveroide <nicolasrivero1994@gmail.com>
2023-01-10 15:25:29 -0400

    fix de todo

--- FrontEnd/redux/actions/business/postBusiness.js
+++ FrontEnd/redux/actions/business/postBusiness.js
@@ -2,5 +2,4 @@ import axios from "axios";
 
 export const postBusiness = (data) => () => {
   axios.post("http://bookturn-deploy-cthq4.ondigitalocean.app/api/businesses", {data: data}).catch((error) => error);
-  
 };
