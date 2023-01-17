6a3c3981bcad53433e3b01cdd09bcfd45d16824c
AgustinSG01 <sousagodoyagustin@gmail.com>
2023-01-17 18:53:00 +0100

    card de precio

--- FrontEnd/pages/business/prices/index.jsx
+++ FrontEnd/pages/business/prices/index.jsx
@@ -1,74 +1,121 @@
-import Navbar from 'components/Navbar/Navbar'
-import React from 'react'
-import styles from "../../../styles/BusinessPrices.module.css"
+import Navbar from "components/Navbar/Navbar";
+import Link from "next/link";
+import React from "react";
+import styles from "../../../styles/BusinessPrices.module.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const index = () => {
   return (
     <div className={styles.generalContainer}>
       <Navbar />
-      <section>
-        <div className={styles.beforePrices}>
-          <h1>Disfruta de nuestros beneficios premium <br/> para tu empresa</h1>
-        </div>
-      </section>
-      <section className={styles.sectionPrice}>
-        <div className={styles.pricesContainer}>
-          <div className={styles.textPricesContainer}>
-            <div className={styles.titlePrice}>
-            <p>Suscribete a Bookturn</p>
-            </div>
-            <div className={styles.price}>
-            <p className={styles.priceNumber}>$5.00</p>
-            <p>cada mes + iva</p>
-            </div>
+      <section class="bg-white">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+          <div
+            class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12"
+            data-aos="zoom-in"
+          >
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">
+              Registra tu empresa en Bookturn!
+            </h2>
+            <p class="mb-5 font-light text-gray-500 sm:text-xl">
+              Gestiona tus turnos de forma sencilla
+            </p>
           </div>
-          <div className={styles.benefitContainer}>
-            <ul>
-              <li><span><svg className={styles.svgIcon} viewBox="0 0 20 20">
-							<path fill="none" d="M7.197,16.963H7.195c-0.204,0-0.399-0.083-0.544-0.227l-6.039-6.082c-0.3-0.302-0.297-0.788,0.003-1.087
-							C0.919,9.266,1.404,9.269,1.702,9.57l5.495,5.536L18.221,4.083c0.301-0.301,0.787-0.301,1.087,0c0.301,0.3,0.301,0.787,0,1.087
-							L7.741,16.738C7.596,16.882,7.401,16.963,7.197,16.963z"></path>
-						</svg>Beneficio 1</span></li>
-              <li><span><svg className={styles.svgIcon} viewBox="0 0 20 20">
-							<path fill="none" d="M7.197,16.963H7.195c-0.204,0-0.399-0.083-0.544-0.227l-6.039-6.082c-0.3-0.302-0.297-0.788,0.003-1.087
-							C0.919,9.266,1.404,9.269,1.702,9.57l5.495,5.536L18.221,4.083c0.301-0.301,0.787-0.301,1.087,0c0.301,0.3,0.301,0.787,0,1.087
-							L7.741,16.738C7.596,16.882,7.401,16.963,7.197,16.963z"></path>
-						</svg>Beneficio 2</span></li>
-              <li><span><svg className={styles.svgIcon} viewBox="0 0 20 20">
-							<path fill="none" d="M7.197,16.963H7.195c-0.204,0-0.399-0.083-0.544-0.227l-6.039-6.082c-0.3-0.302-0.297-0.788,0.003-1.087
-							C0.919,9.266,1.404,9.269,1.702,9.57l5.495,5.536L18.221,4.083c0.301-0.301,0.787-0.301,1.087,0c0.301,0.3,0.301,0.787,0,1.087
-							L7.741,16.738C7.596,16.882,7.401,16.963,7.197,16.963z"></path>
-						</svg>Beneficio 3</span></li>
-              <li><span><svg className={styles.svgIcon} viewBox="0 0 20 20">
-							<path fill="none" d="M7.197,16.963H7.195c-0.204,0-0.399-0.083-0.544-0.227l-6.039-6.082c-0.3-0.302-0.297-0.788,0.003-1.087
-							C0.919,9.266,1.404,9.269,1.702,9.57l5.495,5.536L18.221,4.083c0.301-0.301,0.787-0.301,1.087,0c0.301,0.3,0.301,0.787,0,1.087
-							L7.741,16.738C7.596,16.882,7.401,16.963,7.197,16.963z"></path>
-						</svg>Beneficio 4</span></li>
-              <li><span><svg className={styles.svgIcon} viewBox="0 0 20 20">
-							<path fill="none" d="M7.197,16.963H7.195c-0.204,0-0.399-0.083-0.544-0.227l-6.039-6.082c-0.3-0.302-0.297-0.788,0.003-1.087
-							C0.919,9.266,1.404,9.269,1.702,9.57l5.495,5.536L18.221,4.083c0.301-0.301,0.787-0.301,1.087,0c0.301,0.3,0.301,0.787,0,1.087
-							L7.741,16.738C7.596,16.882,7.401,16.963,7.197,16.963z"></path>
-						</svg>Beneficio 5</span></li>
-              <li><span><svg className={styles.svgIcon} viewBox="0 0 20 20">
-							<path fill="none" d="M7.197,16.963H7.195c-0.204,0-0.399-0.083-0.544-0.227l-6.039-6.082c-0.3-0.302-0.297-0.788,0.003-1.087
-							C0.919,9.266,1.404,9.269,1.702,9.57l5.495,5.536L18.221,4.083c0.301-0.301,0.787-0.301,1.087,0c0.301,0.3,0.301,0.787,0,1.087
-							L7.741,16.738C7.596,16.882,7.401,16.963,7.197,16.963z"></path>
-						</svg>Beneficio 6</span></li>
-            </ul>
-            <button className={styles.buyButtonContainer}>
-            <svg className={styles.buyButton} viewBox="0 0 20 20">
-							<path fill="none" d="M17.696,9.368H2.305c-0.189,0-0.367,0.092-0.478,0.245c-0.11,0.155-0.141,0.352-0.08,0.532l2.334,6.918c0.081,0.238,0.305,0.4,0.556,0.4h10.735c0.253,0,0.478-0.162,0.557-0.402l2.323-6.917c0.062-0.179,0.03-0.376-0.079-0.531C18.062,9.459,17.886,9.368,17.696,9.368z M14.95,16.287H5.062l-1.938-5.743h13.753L14.95,16.287z"></path>
-							<path fill="none" d="M6.345,7.369c0.325,0,0.588-0.263,0.588-0.588c0-1.691,1.376-3.067,3.067-3.067c1.691,0,3.067,1.376,3.067,3.067c0,0.325,0.264,0.588,0.588,0.588c0.326,0,0.589-0.263,0.589-0.588c0-2.34-1.904-4.243-4.244-4.243c-2.34,0-4.244,1.903-4.244,4.243C5.757,7.106,6.02,7.369,6.345,7.369z"></path>
-						</svg>
-            </button>
-          </div>
-        <div>
-
+          <div
+            class="space-y-8 lg:grid lg:grid-cols-1 sm:gap-6 xl:gap-10 lg:space-y-0"
+            data-aos="zoom-in"
+          >
+            {/* <!-- Pricing Card --> */}
+            <div class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow">
+              <h3 class="mb-4 text-2xl font-semibold">Suscripción</h3>
+              <p class="font-light text-gray-500 sm:text-lg">
+                Accede a los beneficios de registrar tu empresa
+              </p>
+              <div class="flex justify-center items-baseline my-8">
+                <span class="mr-2 text-5xl font-extrabold">$20</span>
+                <span class="text-gray-500">/mes</span>
+              </div>
+              {/* <!-- List --> */}
+              <ul role="list" class="mb-8 space-y-4 text-left">
+                <li class="flex items-center space-x-3">
+                  {/* <!-- Icon --> */}
+                  <svg
+                    class="flex-shrink-0 w-5 h-5 text-green-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span>Gestion de turnos</span>
+                </li>
+                <li class="flex items-center space-x-3">
+                  {/* <!-- Icon --> */}
+                  <svg
+                    class="flex-shrink-0 w-5 h-5 text-green-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span>Visibilidad</span>
+                </li>
+                <li class="flex items-center space-x-3">
+                  {/* <!-- Icon --> */}
+                  <svg
+                    class="flex-shrink-0 w-5 h-5 text-green-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span>Dashboard personal</span>
+                </li>
+                <li class="flex items-center space-x-3">
+                  {/* <!-- Icon --> */}
+                  <svg
+                    class="flex-shrink-0 w-5 h-5 text-green-500"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span>Comodidad</span>
+                </li>
+              </ul>
+              <Link
+                href="/business/login/createform"
+                class="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Suscribete
+              </Link>
+            </div>
+            {/* <!-- Pricing Card --> */}
           </div>
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;
