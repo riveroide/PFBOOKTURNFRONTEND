1f955a16e5b9235ef7fb40e602a19adb0306e44b
riveroide <nicolasrivero1994@gmail.com>
2023-01-20 00:57:47 -0400

    fix booking

--- FrontEnd/components/BookingSteps/Steps/Step3.jsx
+++ FrontEnd/components/BookingSteps/Steps/Step3.jsx
@@ -9,8 +9,8 @@ const Step3 = ({ finaldata, setfinaldata }) => {
 
   return (
     <div className="flex justify-center cursor-pointer truncate text-3xl font-medium text-gray-500 items-center flex-col font-cool_p h-max py-20">
-      <h3 className="font-black">Fecha y horario reservado:</h3> {finaldata.dateinfo}
-      <h3 className="font-black">Servicios: </h3>
+      <h3 className="font-black uppercase tracking-wider">Fecha y horario reservado:</h3> {finaldata.dateinfo}
+      <h3 className="font-black uppercase tracking-wider">Servicio/s: </h3>
       <ul>
       {finaldata.services?.map((e)=>{ 
       return (<li>{e}</li>)
