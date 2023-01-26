0f1e6a2a37a56abc4183895083f5a136e1de761b
riveroide <nicolasrivero1994@gmail.com>
2023-01-25 22:27:37 -0400

    fix formulario business

--- FrontEnd/components/BusinessSteps/Steps/Step2.jsx
+++ FrontEnd/components/BusinessSteps/Steps/Step2.jsx
@@ -55,7 +55,7 @@ const Step2 = ({
   const dispatch = useDispatch();
   const { userInfo } = useSelector((state) => state.users);
   const [errors, setErrors] = useState({});
-  console.log(finalData, "soy final data");
+  console.log(userInfo[0]?.id, "soy userinfoID");
 
   const [input, setInput] = useState({
     name: "",
